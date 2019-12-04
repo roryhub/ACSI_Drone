597b1fc50dcc949faa3b146c3095d5a36f367166
Grant Metts <gmetts@andrew.cmu.edu>
2019-12-04 14:14:49 -0500

    Added Crazyflie Matlab to Repo

---
+++ Crazyflie_Matlab/init.m
@@ -0,0 +1,4 @@
+run('init_quadcopter_model');
+run('init_inner_controller');
+fprintf(['Proceed by running the init file in any sub directory,\n',...
+         'followed by running the corresponding model.\n'])
