fixed q calculation according to Kevin's script

mpc.py
@@ -43,7 +43,7 @@ class MPC:
         
         self.P = self.build_P(Rbar, RbarD, Qbar, Su, L)
 
-        self.Fu, self.Fr, self.Fx = self.build_Fs(Rbar, Qbar, Su, Sx, L)
+        self.Fu1, self.Fu2, self.Fr, self.Fx = self.build_Fs(Rbar, Qbar, Su, Sx, L)
         
         self.W0 = self.build_W0(self.N, umin, umax)
 
@@ -166,20 +166,24 @@ class MPC:
 
     
     def build_Fs(self, Rbar, Qbar, Su, Sx, L):
-        Fu = 2 * (Su.T @ Qbar.T @ Su[:, :self.num_inputs]
-                + np.tile(np.diag(L.T @ Rbar.T)[:, np.newaxis], (1, self.num_inputs)))
+        Fu1 = 2 * (Rbar.T @ L.T)
+        Fu2 = 2 * (Su[:, :self.num_inputs].T @ Qbar @ Su).T
         Fr = -2 * (Su.T @ Qbar.T)
         Fx = 2 * (Su.T @ Qbar.T @ Sx)
 
-        assert Fu.shape == (self.num_inputs * self.N, self.num_inputs)
+        assert Fu1.shape == (self.num_inputs * self.N, self.num_inputs * self.N)
+        assert Fu2.shape == (self.num_inputs * self.N, self.num_inputs)
         assert Fr.shape == (self.num_inputs * self.N, self.num_outputs * self.N)
         assert Fx.shape == (self.num_inputs * self.N, self.num_states)
 
-        return Fu, Fr, Fx
+        return Fu1, Fu2, Fr, Fx
 
 
     def calculate_q(self, X, U, traj_horizon):
-        q = self.Fx @ X + self.Fu @ U + self.Fr @ traj_horizon
+        q = self.Fx @ X \
+            + self.Fu2 @ U \
+            + self.Fu1 @ np.tile(U, (self.N, 1)) \
+            + self.Fr @ traj_horizon
 
         assert q.shape == (self.num_inputs * self.N, 1)
 

simulator.py
@@ -34,8 +34,8 @@ class Simulator:
         else:
             rx = 3 * np.ones(len(self.t))
             ry = np.ones(len(self.t))
-            rz = signal.square(self.t / 16)
-            ryaw = 2 * signal.square(self.t / 16)
+            rz = signal.square(self.t / 6)
+            ryaw = 2 * signal.square(self.t / 6)
 
             self.ref_traj = np.row_stack((rx, ry, rz, ryaw))
 
@@ -61,7 +61,7 @@ class Simulator:
             U = self.mpc.get_control_input(X, U, remaining_traj)
 
             X = self.update_states(X, U)
-    
+
 
     def establish_starting_state(self):
         U = np.zeros((self.B.shape[1], 1))
@@ -175,7 +175,7 @@ def main(model_type=0):
         B = matfile['B']
         C = matfile['C']
 
-        Q = np.diag(np.array([1000., 1000., 1000., 1000.]))
+        Q = np.diag(np.array([1000., 10000., 1000., 1000.]))
         R = np.diag(np.array([1., 1., 1., 1e-8]))
         RD = np.diag(np.array([.01, .01, .1, 1e-8]))
 
@@ -186,7 +186,7 @@ def main(model_type=0):
 
     sim = Simulator(A, B, C, Q, R, RD, umin, umax, N)
 
-    traj_length = 4*N
+    traj_length = 4 * N
 
     sim.get_reference_trajectory(traj_length, model_type=model_type)
 
@@ -197,5 +197,5 @@ def main(model_type=0):
 
 if __name__ == '__main__':
 
-    mtype = 1
+    mtype = 1p
     main(mtype)