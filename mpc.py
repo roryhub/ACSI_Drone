c69d9fbea707db25da9b77158bebe2bc3546ac53
Rory <chubbard@alumni.cmu.edu>
2019-12-08 03:26:48 -0500

    implemented constraint matrices for MIMO

--- mpc.py
+++ mpc.py
@@ -6,15 +6,30 @@ from cvxopt import matrix, solvers
 
 class MPC:
 
-    def __init__(self, A, B, C, Q, R, RD, N):
+    def __init__(self, A, B, C, Q, R, RD, umin, umax, N):
         solvers.options['show_progress'] = False
 
         self.N = N # horizon length
+        B, C = self.dimension_fill(B, C)
 
-        self.precompute(A, B, C, Q, R, RD)
+        self.num_states = A.shape[1]
+        self.num_inputs = B.shape[1]
+        self.num_outputs = C.shape[0]
+
+        self.precompute(A, B, C, Q, R, RD, umin, umax)
 
     
-    def precompute(self, A, B, C, Q, R, RD):
+    def dimension_fill(self, B, C):
+        if B.ndim < 2:
+            B = B[:, np.newaxis]
+
+        if C.ndim < 2:
+            C = C[np.newaxis, :]
+        
+        return B, C
+
+    
+    def precompute(self, A, B, C, Q, R, RD, umin, umax):
 
         Qbar, Rbar, RbarD = self.build_bars(Q, R, RD, self.N)
 
@@ -30,66 +45,118 @@ class MPC:
 
         self.Fu, self.Fr, self.Fx = self.build_Fs(Rbar, Qbar, Su, Sx, L)
         
-        self.W0 = self.build_W0(self.N)
+        self.W0 = self.build_W0(self.N, umin, umax)
 
         self.S = self.build_S(self.N)
     
 
     def build_bars(self, Q, R, RD, N):
-        Qbar = np.diag(Q * np.ones(N))
-        Rbar = np.diag(R * np.ones(N))
-        RbarD = np.diag(RD * np.ones(N))
+        Qbar = np.kron(np.eye(self.num_outputs * N), Q) # weight penalty for states
+        Rbar = np.kron(np.eye(self.num_outputs * N), R) # weight penalty for control effort
+        RbarD = np.kron(np.eye(self.num_outputs * N), RD) # weight penalty for change in control effort
+
+        assert Qbar.shape == (self.num_outputs * N, self.num_outputs * N)
+        assert Rbar.shape == (self.num_inputs * N, self.num_inputs * N)
+        assert RbarD.shape == (self.num_inputs * N, self.num_inputs * N)
 
         return Qbar, Rbar, RbarD
 
 
     def build_Sx(self, A, C, N):
+        '''
+            Sx is how the initial state propogates to the future predicted outputs
+            [ C * A
+              C * A^2
+                :
+              C * A^N ]
+        '''
+        '''
+            This commented code is a more intuitive but slower way to build Sx
+        '''
+        # Sx1 = C @ A
+        # for i in range(2, N+1):
+        #     next_element = C @ np.linalg.matrix_power(A, i)
+        #     Sx1 = np.row_stack((Sx1, next_element))
+
         Sx = np.array([
             C @ np.linalg.matrix_power(A, i)
             for i in range(1, N+1)
-        ])
+        ]).reshape(self.num_outputs * N, self.num_states)
+
+        assert Sx.shape == (self.num_outputs * N, self.num_states)
 
         return Sx
     
 
     def build_Su(self, A, B, C, N):
-        Su1 = np.cumsum([
+        '''
+            S is how the history of control inputs propogate to the future predicted outputs
+        '''
+
+        Su1 = np.array([
             C @ np.linalg.matrix_power(A, i) @ B
             for i in range(N)
-        ])
+        ]).reshape(self.num_outputs * N, self.num_inputs)
+
+        assert Su1.shape == (self.num_outputs * N, self.num_inputs)
+        
+        '''
+            This commented code is a more intuitive but slower way to build Su2
+        '''
+        # zero_array = np.zeros((self.num_outputs, self.num_inputs))
+        # Su2 = np.concatenate((zero_array, Su1[:-4,:]))
+        # for i in range(2, N):
+        #     column_top = np.tile(zero_array, (i, 1))
+        #     next_column = np.concatenate((column_top, Su1[:-i*self.num_outputs,:]))
+        #     Su2 = np.column_stack((Su2, next_column))
 
         Su2 = np.array([
-            np.concatenate((i*[0], Su1[:-i]))
+            np.concatenate((np.tile(np.zeros((self.num_outputs, self.num_inputs)),(i,1)), Su1[:-i*self.num_outputs,:])).T
             for i in range(1, N)
-        ]).T
+        ]).reshape(self.num_inputs * (N - 1), self.num_outputs * N).T
+
+        assert Su2.shape == (self.num_outputs * N, self.num_inputs * (N - 1))
+        assert np.allclose(Su2[self.num_outputs:, :self.num_inputs], Su1[:-self.num_outputs, :])
+        assert np.allclose(Su2[-self.num_outputs:, -self.num_inputs:], Su1[:self.num_outputs, :])
 
         Su = np.column_stack((Su1, Su2))
+
+        assert Su.shape == (self.num_outputs * N, self.num_inputs * N)
 
         return Su
 
     
     def build_L(self, N):
-        L = np.tril(np.ones(N))
+        L = np.tril(np.tile(np.eye(self.num_inputs), (N, N)))
+
+        assert L.shape == (self.num_inputs * N, self.num_inputs * N)
 
         return L
 
     
     def build_G(self, L, N):
-        upper_control_limit = L
-        lower_control_limit = np.negative(L)
-        G = np.row_stack((upper_control_limit, lower_control_limit))
+        G = np.row_stack((L, np.negative(L)))
+
+        assert G.shape == (2 * self.num_inputs * N, self.num_inputs * N)
 
         return G
 
     
-    def build_W0(self, N):
-        W0 = np.ones(2*N)
+    def build_W0(self, N, umin, umax):
+        # self.W01 = np.ones(2 * self.num_inputs * N)[:, np.newaxis]
+        umin_arr = np.tile(np.negative(umin), (N, 1))
+        umax_arr = np.tile(umax, (N, 1))
+        W0 = np.row_stack((umax_arr, umin_arr))
+
+        assert W0.shape == (2 * self.num_inputs * N, 1)
 
         return W0
 
     
     def build_S(self, N):
-        S = np.zeros((2*N, 2))
+        S = np.zeros((2 * self.num_inputs * N, self.num_states))
+
+        assert S.shape == (2 * self.num_inputs * N, self.num_states)
 
         return S
 
@@ -97,18 +164,55 @@ class MPC:
     def build_P(self, Rbar, RbarD, Qbar, Su, L):
         P = 2 * (Su.T @ Qbar @ Su + L.T @ Rbar @ L + RbarD)
 
+        assert P.shape == (self.num_inputs * self.N, self.num_inputs * self.N)
+
         return P
 
     
     def build_Fs(self, Rbar, Qbar, Su, Sx, L):
-        Fu = 2 * (Su.T @ Qbar.T @ Su[:,0] + np.diag(L.T @ Rbar.T))[:, np.newaxis]
+        Fu = 2 * (Su.T @ Qbar.T @ Su[:, :self.num_inputs]
+                + np.tile(np.diag(L.T @ Rbar.T)[:, np.newaxis], (1, self.num_inputs)))
         Fr = -2 * (Su.T @ Qbar.T)
         Fx = 2 * (Su.T @ Qbar.T @ Sx)
+
+        assert Fu.shape == (self.num_inputs * self.N, self.num_inputs)
+        assert Fr.shape == (self.num_inputs * self.N, self.num_outputs * self.N)
+        assert Fx.shape == (self.num_inputs * self.N, self.num_states)
 
         return Fu, Fr, Fx
 
 
+    def calculate_q(self, X, U, traj_horizon):
+        q = self.Fx @ X + self.Fu @ U + self.Fr @ traj_horizon
+
+        assert q.shape == (self.num_inputs * self.N, self.num_inputs)
+
+        return q
+
+
+    def calculate_W(self, U):
+        # ones_array = np.tile(np.ones(self.N)[:, np.newaxis], (1, self.num_inputs))
+        # low_limit = ones_array @ np.negative(U)
+        # high_limit = ones_array @ U
+        # W1 = self.W01 + np.concatenate((low_limit, high_limit))
+        U_pos = np.tile(U, (self.N, 1))
+        U_neg = np.tile(np.negative(U), (self.N, 1))
+        W = self.W0 - np.concatenate((U_pos, U_neg))
+
+        assert W.shape == (2 * self.num_inputs * self.N, 1)
+
+        return W
+
+
+    def calculate_h(self, W, X):
+        h = W + self.S @ X
+
+        return h
+
+    
     def get_control_input(self, X, U, traj):
+        U, traj = self.fix_dimensions(U, traj)
+
         traj_horizon = self.get_trajectory_horizon(traj)
 
         q = self.calculate_q(X, U, traj_horizon)
@@ -119,39 +223,39 @@ class MPC:
 
         sol = solvers.qp(matrix(self.P), matrix(q), matrix(self.G), matrix(h))
 
-        Uarr = np.array(sol['x'])
-        U += Uarr[0][0]
+        future_control = np.array(sol['x'])
+
+        U += future_control[0][:, np.newaxis]
 
         return U
 
+    
+    def fix_dimensions(self, U, traj):
+        # dimension fill if single input system
+        if U.ndim < 2:
+            U = U[:, np.newaxis]
+        
+        # dimension fill if trajectory is 1D
+        if traj.ndim < 2:
+            traj = traj[np.newaxis, :]
 
-    def calculate_q(self, X, U, ref_horizon):
-        q = self.Fx @ X + self.Fu * U + self.Fr @ ref_horizon
-
-        return q
-
-
-    def calculate_W(self, U):
-        low_limit = np.ones(self.N) * -U
-        high_limit = np.ones(self.N) * U
-        W = (self.W0 + np.concatenate((low_limit, high_limit)))[:, np.newaxis]
-
-        return W
-
-
-    def calculate_h(self, W, X):
-        h = W + self.S @ X
-
-        return h
+        return U, traj
 
 
     def get_trajectory_horizon(self, traj):
         # if horizon is greater than trajectory length, increase trajectory by stacking the last position
-        delta = self.N - len(traj)
+        delta = self.N - traj.shape[1]
         if delta > 0:
-            end_position = np.array([traj[-1]])
-            traj = np.concatenate((traj, np.tile(end_position, delta)))
-
-        traj_horizon = traj[:self.N][:, np.newaxis]
+            end_position = traj[:, -1][:, np.newaxis]
+            traj = np.column_stack((traj, np.tile(end_position, (1, delta))))
+        
+        traj = self.stack_trajectories(traj)
+        traj_horizon = traj[:self.num_inputs * self.N]
 
         return traj_horizon
+    
+
+    def stack_trajectories(self, traj):
+        traj = traj.T.reshape(-1,1)
+        
+        return traj
--- simulator.py
+++ simulator.py
@@ -2,36 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from scipy import signal
+from scipy.io import loadmat
+import os
 
 from mpc import MPC
 
 
 class Simulator:
 
-    def __init__(self, A, B, C, Q, R, RD, N):
+    def __init__(self, A, B, C, Q, R, RD, umin, umax, N):
         self.A = A
         self.B = B
 
-        self.mpc = MPC(A, B, C, Q, R, RD, N)
+        self.mpc = MPC(A, B, C, Q, R, RD, umin, umax, N)
 
         self.t = None
         self.ref_traj = None
 
 
-    def get_reference_trajectory(self, n):
+    def get_reference_trajectory(self, n, model_type=0):
         self.t = np.linspace(1, n, n)
-        self.ref_traj = signal.square(self.t / 6)
+
+        if model_type == 0:
+            self.ref_traj = signal.square(self.t / 6)
+        
+        else:
+            first = signal.square(self.t / 6)
+            second = np.zeros((len(first)))
+            third = np.zeros((len(first)))
+            fourth = np.zeros((len(first)))
+
+            self.ref_traj = np.row_stack((first, second, third, fourth))
 
     
     def simulate(self):
-        U = 0
 
-        X = np.array([0.0, 0.0])[:, np.newaxis]
+        U, X = self.establish_starting_state()
 
         for i in range(len(self.ref_traj)):
 
             if i == 0:
-                state_history = np.array([0.0, 0.0])[:, np.newaxis]
+                state_history = X
             else:
                 state_history = np.column_stack((state_history, X))
             
@@ -42,44 +53,83 @@ class Simulator:
             X = self.update_states(X, U)
 
 
-        return state_history
+        self.state_history = state_history
+    
+
+    def establish_starting_state(self):
+        U = np.zeros((self.B.shape[1], 1))
+        X = np.zeros((self.A.shape[1], 1))
+
+        return U, X
     
 
     def update_states(self, X, U):
-        X = self.A @ X + (self.B * U)[:, np.newaxis]
+        X = self.A @ X + self.B @ U
         
         return X
 
 
-def main():
+    def plot(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.t, self.ref_traj)
+        ax.plot(self.t, self.state_history[0,:])
 
-    A = np.array([
-        [1.0, 1.0],
-        [0.0, 1.0]
-    ])
-    B = np.array([0.0, 1.0])
-    C = np.array([1.0, 0.0])
+        plt.show()
 
-    Q = 1.0
-    R = 0.1
-    RD = 1.0
+
+def main(model_type=0):
+
+    if model_type == 0: # SISO
+        A = np.array([
+            [1.0, 1.0],
+            [0.0, 1.0]
+        ])
+        B = np.array([0.0, 1.0]).reshape(-1,1)
+        C = np.array([1.0, 0.0]).reshape(1,-1)
+
+        Q = np.array([1.0])
+        R = np.array([0.1])
+        RD = np.array([1.0])
+
+        umin = np.array([-1.0])
+        umax = np.array([1.0])
+
+    else: # MIMO
+        directory = 'Crazyflie_Matlab'
+        fname = 'Rory.mat'
+
+        full_path = os.path.join(directory, fname)
+
+        matfile = loadmat(full_path)
+
+        A = matfile['A']
+        B = matfile['B']
+        C = matfile['C']
+
+        nout = C.shape[1]
+        nin = B.shape[1]
+
+        Q = np.array([1.0] * nout)
+        R = np.array([0.1] * nin)
+        RD = np.array([1.0] * nin)
+
+        umin = np.tile(np.array([-1.0]),(nin, 1))
+        umax = np.tile(np.array([1.0]),(nin, 1))
+
     N = 100
 
-    sim = Simulator(A, B, C, Q, R, RD, N)
+    sim = Simulator(A, B, C, Q, R, RD, umin, umax, N)
 
-    traj_length = N
+    traj_length = 2*N
 
-    sim.get_reference_trajectory(traj_length)
+    sim.get_reference_trajectory(traj_length, model_type=model_type)
 
-    state_history = sim.simulate()
+    sim.simulate()
 
-    fig, ax = plt.subplots()
-    ax.plot(sim.t, sim.ref_traj)
-    ax.plot(sim.t, state_history[0,:])
-
-    plt.show()
+    sim.plot()
 
 
 if __name__ == '__main__':
 
-    main()
+    mtype = 0
+    main(mtype)
